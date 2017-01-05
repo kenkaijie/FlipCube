7500f89f0ef38a60972752ae31c196aa1d027d58
Kenneth Kaijie Ng <ngkaijie@hotmail.com>
2017-01-05 21:49:19 +1100

    - Reworked player and game grid controller into State Machines - Cleaned up player controller code - Added game grid generation and win/lose checking

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,15 +2,201 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
+    public enum GameControllerState
+    {
+        CREATED, // created
+        IDLE, // ready to get input
+        BUSY, // busy processing input, prevent any more input from occuring
+		WIN,
+		LOSE
+    }
+
+    // game objects
+    public GameObject gameTilePrefab;
+    public GameObject playerCubePrefab;
+
+    public List<Vector3> playArea;
+    private List<GameObject> gameTiles = new List<GameObject>();
+    private GameObject playerCube;
+
+    // controller state
+    private GameControllerState state = GameControllerState.CREATED;
+    
+    // Use this for initialization
 	void Start () {
-		
+		foreach (Vector3 point in playArea)
+        {
+            Debug.Log("Generating a tile at " + point.ToString());
+            GameObject a = Instantiate<GameObject>(gameTilePrefab, point, Quaternion.identity);
+            a.transform.parent = gameObject.transform;
+            gameTiles.Add(a);
+        }
+
+		playerCube = Instantiate<GameObject>(playerCubePrefab, Vector3.zero, Quaternion.identity);
+		playerCube.transform.parent = gameObject.transform;
+
+        TransitionToState(GameControllerState.IDLE);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        switch (state)
+        {
+        case GameControllerState.CREATED:
+            // do nothing
+            break;
+        case GameControllerState.IDLE:
+            // in idle we check inputs
+            CheckInputs();
+            break;
+        case GameControllerState.BUSY:
+            break;
+		case GameControllerState.LOSE:
+			break;
+		case GameControllerState.WIN:
+			break;
+        }
+
+    }
+
+    private bool CheckInputs()
+    {
+        PlayerController.TumblingDirection dir = PlayerController.TumblingDirection.NONE;
+		PlayerController playerController = playerCube.GetComponent<PlayerController>();
+
+        if (Input.GetButton("Up"))
+        {
+            dir = PlayerController.TumblingDirection.UP;
+        }
+        else if (Input.GetButton("Down"))
+        {
+            dir = PlayerController.TumblingDirection.DOWN;
+        }
+        else if (Input.GetButton("Left"))
+        {
+            dir = PlayerController.TumblingDirection.LEFT;
+        }
+        else if (Input.GetButton("Right"))
+        {
+            dir = PlayerController.TumblingDirection.RIGHT;
+        }
+        else
+        {
+            return false;
+        }
+			
+        if (playerController.SetTumblingDirection(dir, OnPlayerTumbleFinish))
+        {
+            TransitionToState(GameControllerState.BUSY);
+        }
+
+        return true;
+    }
+
+    void OnPlayerTumbleFinish()
+    {
+		TransitionToState(GameControllerState.IDLE);
+		Vector3 tilePosition = playerCube.GetComponentInChildren<PlayerController>().targetPosition - 0.5f * Vector3.up;
+		if (CheckBounds(tilePosition))
+		{	
+			CheckWin();
+		}
+		else
+		{
+			playerCube.GetComponent<PlayerController>().FreezePlayer();
+			TransitionToState(GameControllerState.LOSE);
+		}
+
+    }
+
+	private bool CheckBounds(Vector3 playerPosition)
+	{
+		int nearby = 0;
+		foreach (Vector3 point in playArea)
+		{
+			if ((playerPosition-point).sqrMagnitude <= 0.1)
+			{
+				nearby += 1;
+			}
+		}
+
+		if (nearby == 1) 
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
+
+    private void CheckWin()
+    {
+		Vector3 tilePosition = playerCube.GetComponentInChildren<PlayerController>().targetPosition - 0.5f * Vector3.up;
+		int tileCount = 0;
+		foreach (GameObject tile in gameTiles)
+		{
+			if (tile.GetComponent<GameTileController>().tileState == GameTileController.GameTileState.ACTIVE)
+			{
+				tileCount += 1;
+			}
+		}
+
+		if (tileCount == playArea.Count) 
+		{
+			TransitionToState(GameControllerState.WIN);
+		}
+    }
+
+    private void TransitionToState(GameControllerState newState)
+    {
+        Debug.Log("Transitioning state (" + state.ToString() + " -> " + newState.ToString() + ")", gameObject);
+        switch (newState)
+        {
+            case GameControllerState.CREATED:
+                // do nothing, we cant transition back to this state
+                break;
+            case GameControllerState.IDLE:
+                if (state == GameControllerState.CREATED || state == GameControllerState.BUSY)
+                {
+                    state = GameControllerState.IDLE;
+                }
+                else
+                {
+                    // do nothing, will get caught
+                }
+                break;
+            case GameControllerState.BUSY:
+                if (state == GameControllerState.IDLE)
+                {
+                    state = GameControllerState.BUSY;
+                }
+                else
+                {
+                    // do nothing, will get caught
+                }
+                break;
+		case GameControllerState.LOSE:
+			if (state == GameControllerState.IDLE) 
+			{
+				state = GameControllerState.LOSE;
+			}
+			break;
+		case GameControllerState.WIN:
+			if (state == GameControllerState.IDLE) 
+			{
+				state = GameControllerState.WIN;
+			}
+			break;
+        }
+        if (state != newState)
+        {
+            Debug.LogError("Incorrect transition. ", gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/GameTileController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameTileController : MonoBehaviour
+{
+    public enum GameTileState
+    {
+        ACTIVE,
+        INACTIVE
+    }
+
+    public GameObject lightingCube;
+    public GameObject lightingBack;
+
+    public Renderer lightingCubeTexture;
+
+    public Color on;
+    public Color off;
+
+    public GameTileState tileState;
+
+    private GameTileState lastProcessedState;
+
+    // RED E1212180
+    // GREEN 86F48C80
+    // BLUE 21B9E180
+
+    // Use this for initialization
+    void Start()
+    {
+        tileState = GameTileState.INACTIVE;
+        lastProcessedState = GameTileState.ACTIVE;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tileState != lastProcessedState)
+        {
+
+            switch (tileState)
+            {
+                case GameTileState.ACTIVE:
+                    lightingCubeTexture.material.color = on;
+                    break;
+                case GameTileState.INACTIVE:
+                    lightingCubeTexture.material.color = off;
+                    break;
+            }
+            lastProcessedState = tileState;
+        }
+        
+    }
+
+    private void ToggleTileState()
+    {
+        switch (tileState)
+        {
+            case GameTileState.ACTIVE:
+                tileState = GameTileState.INACTIVE;
+                break;
+            case GameTileState.INACTIVE:
+                tileState = GameTileState.ACTIVE;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ToggleTileState();
+    }
+
+}
